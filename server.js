c933ff75049d19db749ef6a77a6a22689b628cda
Aadhil Kassim <aadhikassim@gmail.com>
2025-06-11 12:53:20 +0530

    API created

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());         
+app.use(express.json());
+
+app.get('/getData/:id', (req, res) => {
+    const userData = {
+        name: req.params.id,
+        age: 30,
+        email: 'johndoe@mail.com'
+    };
+
+    res.send(userData);
+});
+
+app.post('/createData', (req, res) => {
+    const data = req.body;
+    console.log('Data received:', data);
+    res.send("Data created successfully");
+});
+
+
+
+const port = 8888;
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+  console.log('Visit http://localhost:' + port);
+});
