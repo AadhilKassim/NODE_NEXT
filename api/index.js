fb497a0a926f4fa6ed3fb9f72af1e156a2381815
Aadhil Kassim <aadhikassim@gmail.com>
2025-06-17 16:05:58 +0530

    Added BCRYPT, body-parser, and cookie-parser
    Used for parsing the data passed to the server and to be used to encrypt the password before it being saved to the DB

--- api/index.js
+++ api/index.js
@@ -1,37 +1,56 @@
+const connectToMongoDB = require('../MongoDB/mongoDB-config');
+
 const express = require('express');
 const cors = require('cors');
+const { default: userModel } = require('../MongoDB/mongoDB-model');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+
 const app = express();
 app.use(cors());         
 app.use(express.json());
 
-app.get('/', (res, req) =>{
-    const upMessage = {
-        message: 'Welcome to the API',
-        status: 'success'
-    };
-    res.send(upMessage);
-});
 
-app.get('/getData/:id', (req, res) => {
-    const userData = {
-        name: req.params.id,
-        age: 30,
-        email: 'johndoe@mail.com'
-    };
+// userModel.create({
+//     name: 'John Doe',
+//     age: 30,
+//     place: 'New York'
+// });
 
-    res.send(userData);
-});
 
-app.post('/createData', (req, res) => {
-    const data = req.body;
-    console.log('Data received:', data);
-    res.send("Data created successfully");
-});
+// app.get('/getData/:id', (req, res) => {
+//     const userData = {
+//         name: req.params.id,
+//         age: 30,
+//         email: 'johndoe@mail.com'
+//     };
+    
+//     res.send(userData);
+// });
 
+// app.post('/createData', async (req, res) => {
+//     const data = req.body;
+//     const newUser = await userModel.create(data);
+//     console.log('Data received:', data);
+//     res.send("Data created successfully" + JSON.stringify(newUser));
+// });
+
+app.post('/signup', (req, res) => {
+    const {name, email, password} = req.body;
+    const hashedpassword = bcrypt.hashSync(password, 10);
+    const user = userModel({name, email, password: hashedpassword});
+    user.save().then((UserInfo) => {
+        console.log('User created successfully:', UserInfo);
+        res.send('')
+    })
+})
 
 
 const port = 8888;
 app.listen(port, () => {
-  console.log('Server is running on port', port);
-  console.log('Visit http://localhost:' + port);
+    console.log('Server is running on port', port);
+    console.log('Visit http://localhost:' + port);
 });
+connectToMongoDB();
